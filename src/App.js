4a1b4e17204060feae52a41ecd24949ad4853ffc
anjali-1607 <anjalirajpoot1609@gmail.com>
2023-09-05 19:13:41 +0530

    ui

--- src/App.js
+++ src/App.js
@@ -2,5 +2,17 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div>App</div>;
+  return (
+    <>
+      <div class="container">
+        <h1>Counter App</h1>
+        <div class="counter">
+          <button id="increment-btn">+</button>
+          <div id="counter-value">0</div>
+          <button id="decrement-btn">-</button>
+        </div>
+        <button id="reset">Reset</button>
+      </div>
+    </>
+  );
 }
